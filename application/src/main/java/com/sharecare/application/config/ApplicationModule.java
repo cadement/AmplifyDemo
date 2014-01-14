1165bc94246974ac94f1a97b485dba3a9b184191
cadement <casey@thedements.com>
2014-01-14 18:03:40 -0500

    Repackaging for blossom and now demo app

---
+++ application/src/main/java/com/sharecare/application/config/ApplicationModule.java
@@ -0,0 +1,21 @@
+package com.sharecare.application.config;
+
+import info.magnolia.module.ModuleLifecycle;
+import info.magnolia.module.ModuleLifecycleContext;
+import info.magnolia.module.blossom.module.BlossomModuleSupport;
+
+public class ApplicationModule extends BlossomModuleSupport implements ModuleLifecycle {
+
+    public void start(ModuleLifecycleContext moduleLifecycleContext) {
+        if (moduleLifecycleContext.getPhase() == ModuleLifecycleContext.PHASE_SYSTEM_STARTUP) {
+            super.initBlossomDispatcherServlet("blossom", "classpath:/blossom-servlet.xml");
+        }
+    }
+
+    public void stop(ModuleLifecycleContext moduleLifecycleContext) {
+        if (moduleLifecycleContext.getPhase() == ModuleLifecycleContext.PHASE_SYSTEM_SHUTDOWN) {
+            super.destroyDispatcherServlets();
+            super.closeRootWebApplicationContext();
+        }
+    }
+}
---
+++ application/src/main/java/com/sharecare/application/config/ApplicationModuleVersionHandler.java
@@ -0,0 +1,6 @@
+package com.sharecare.application.config;
+
+import info.magnolia.module.DefaultModuleVersionHandler;
+
+public class ApplicationModuleVersionHandler extends DefaultModuleVersionHandler {
+}
